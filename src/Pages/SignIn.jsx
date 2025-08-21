f5f8ccb8e16ff2671a0269aab95e0c2558ba2866
jabezdubale <jabezdubale6@gmail.com>
2025-08-21 12:57:24 +0300

    made corrections to sign in page

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -13,34 +13,34 @@ const SignIn = () => {
     console.log("Login is handled");
   };
   return (
-    <div className="bg-main-Background h-screen flex justify-center items-center">
-      <div className="bg-secondary-Background w-3/5 max-w-6xl p-[5%] rounded-4xl flex flex-col gap-3">
-        <Logo size={"h-[75px]"} style="text-4xl" />
+    <div className="bg-main-Background min-h-[100svh] flex justify-center items-center">
+      <div className="bg-secondary-Background w-4/5 md:w-3/5 max-w-6xl py-10 rounded-4xl flex flex-col gap-2">
+        <Logo />
         <p className="text-secondary-headings text-xl py-5 mx-auto text-center w-4/5 lg:w-3/5">
           “It is not a calculated risk if you haven’t calculated it.” — Naved
           Abdali
         </p>
         <form
           onSubmit={handleLogin}
-          className="flex flex-col justify-center items-center gap-8"
+          className="flex flex-col justify-center items-center gap-4"
         >
-          <h1 className="text-primary-headings text-4xl font-bold text-center">
+          <h1 className="text-primary-headings text-2xl md:text-3xl font-bold text-center">
             Log in
           </h1>
-          <div className=" bg-white/30 w-3/5 p-3 rounded-xl text-secondary-headings text-4xl flex justify-start items-center gap-2">
+          <div className="bg-white/30 w-3/5 p-2 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
             <MdOutlineEmail />
             <input
               type="text"
               placeholder="Email Adress"
-              className="text-2xl placeholder-secondary-headings w-full outline-none"
+              className="text-xl placeholder-secondary-headings w-full outline-none"
             />
           </div>
-          <div className="bg-white/30 w-3/5 p-3 rounded-xl text-secondary-headings text-4xl flex justify-start items-center gap-2">
+          <div className="bg-white/30 w-3/5 p-2 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
             <TbLockPassword />
             <input
               type="text"
               placeholder="Password"
-              className="text-2xl placeholder-secondary-headings w-full outline-none"
+              className=" text-xl placeholder-secondary-headings w-full outline-none"
             />
             <FaRegEyeSlash className="mr-2 bg-white/2" />
           </div>
@@ -54,7 +54,7 @@ const SignIn = () => {
           <div className="w-3/5 flex flex-col justify-center items-center gap-3">
             <Buttons
               variant="mainVariant"
-              styles="w-full p-3"
+              styles="w-full p-2"
               label="Sign in"
               LeftIcon={null}
               RightIcon={null}
@@ -66,12 +66,12 @@ const SignIn = () => {
             </p>
           </div>
         </form>
-        <div className="flex flex-col justify-center items-center gap-10">
+        <div className="flex flex-col justify-center items-center gap-5">
           <p className="text-primary-headings text-lg">or continue with</p>
           <div className="w-3/5 flex flex-wrap justify-around items-center">
             <Buttons
               variant="mainVariant"
-              styles="w-2/10 p-5"
+              styles="w-3/10 p-3"
               label=""
               LeftIcon={<FaGoogle size={25} />}
               RightIcon={null}
@@ -80,7 +80,7 @@ const SignIn = () => {
             />
             <Buttons
               variant="mainVariant"
-              styles="w-2/10 p-5"
+              styles="w-3/10 p-3"
               label=""
               LeftIcon={<FaFacebookF size={25} />}
               RightIcon={null}
@@ -89,7 +89,7 @@ const SignIn = () => {
             />
             <Buttons
               variant="mainVariant"
-              styles="w-2/10 p-5"
+              styles="w-3/10 p-3"
               label=""
               LeftIcon={<SiTradingview size={25} />}
               RightIcon={null}
@@ -100,13 +100,13 @@ const SignIn = () => {
         </div>
 
         <div className="flex justify-center items-center">
-          <div className="w-3/5 flex justify-center items-center gap-3 mt-10">
+          <div className=" w-3/5 flex justify-between gap-2 items-center mt-5">
             <p className="text-primary-headings text-lg">
               Don't have an account yet?
             </p>
             <Buttons
               variant="secondaryVariant"
-              styles="w-2/5 p-1"
+              styles="max-w-3/5 w-full p-1"
               label="Register For free"
               LeftIcon={null}
               RightIcon={null}
