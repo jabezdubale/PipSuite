d0fe596588f59fa310ffd622e07dec990f5bd468
jabezdubale <jabezdubale6@gmail.com>
2025-08-21 17:53:57 +0300

    Created the register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Link } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import SignIn from "./Pages/SignIn";
+import Register from "./Pages/Register";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -6,18 +6,37 @@ import { FaRegEyeSlash } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
 import { SiTradingview } from "react-icons/si";
+import { IoPersonOutline } from "react-icons/io5";
+import { RiFileUserLine } from "react-icons/ri";
+import { CiCalendarDate } from "react-icons/ci";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SignIn = () => {
+const Register = () => {
   const [showPass, setShowPass] = useState(false);
+  const [showCPass, setShowCPass] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
+  const [dob, setDob] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
-    console.log(email);
-    console.log(password);
+    if (password === confirmPassword) {
+      console.log(fullName);
+      console.log(userName);
+      console.log(email);
+      console.log(dob);
+      console.log(password);
+      navigate("/dashboard");
+    } else {
+      alert("The passwords do not match");
+    }
   };
+
   return (
     <div className="bg-main-Background min-h-[100svh] flex justify-center items-center py-10">
       <div className="bg-secondary-Background w-4/5 md:w-3/5 max-w-4xl py-10 px-[5%] md:px-[8%] rounded-4xl flex flex-col gap-2">
@@ -27,8 +46,31 @@ const SignIn = () => {
           className="flex flex-col justify-center items-start gap-4"
         >
           <h1 className="text-primary-headings text-2xl md:text-3xl font-bold text-center">
-            Welcome Back
+            Register
           </h1>
+
+          <div className="bg-white/30 w-full py-1 px-4 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
+            <IoPersonOutline />
+            <input
+              type="text"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+              placeholder="Full Name"
+              className="text-lg placeholder-secondary-headings w-full outline-none"
+            />
+          </div>
+          <div className="bg-white/30 w-full py-1 px-4 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
+            <RiFileUserLine />
+            <input
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              required
+              placeholder="User Name"
+              className="text-lg placeholder-secondary-headings w-full outline-none"
+            />
+          </div>
           <div className="bg-white/30 w-full py-1 px-4 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
             <MdOutlineEmail />
             <input
@@ -36,8 +78,19 @@ const SignIn = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              placeholder="Email Adress"
+              placeholder="Email"
               className="text-lg placeholder-secondary-headings w-full outline-none"
+            />
+          </div>
+          <div className="relative bg-white/30 w-full py-1 px-4 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
+            <CiCalendarDate />
+            <input
+              type="date"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+              required
+              placeholder="Date of Birth"
+              className="text-lg placeholder-secondary-headings w-full outline-none [&::-webkit-calendar-picker-indicator]:cursor-pointer [&::-webkit-calendar-picker-indicator]:opacity-0 [&::-webkit-calendar-picker-indicator]:absolute [&::-webkit-calendar-picker-indicator]:w-full [&::-webkit-calendar-picker-indicator]:bg-red-500 [&::-webkit-calendar-picker-indicator]:right-0 "
             />
           </div>
           <div className="bg-white/30 w-full py-1 px-4 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
@@ -46,6 +99,7 @@ const SignIn = () => {
               type={showPass ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              required
               placeholder="Password"
               className=" text-lg placeholder-secondary-headings w-full outline-none"
             />
@@ -54,29 +108,46 @@ const SignIn = () => {
               onClick={() => setShowPass(!showPass)}
             />
           </div>
-          <div className="w-3/5 flex justify-start items-center gap-2">
+          <div className="bg-white/30 w-full py-1 px-4 rounded-xl text-secondary-headings text-3xl flex justify-start items-center gap-2">
+            <TbLockPassword />
+            <input
+              type={showCPass ? "text" : "password"}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+              placeholder="Confirm Password"
+              className=" text-lg placeholder-secondary-headings w-full outline-none"
+            />
+            <FaRegEyeSlash
+              className="mr-2 bg-white/2 cursor-pointer"
+              onClick={() => setShowCPass(!showCPass)}
+            />
+          </div>
+          <div className="w-full flex justify-start items-center gap-2 mb-5 mt-2">
             <input
               type="checkbox"
               className="h-5 w-5 accent-brand-green cursor-pointer"
             />
-            <p className="text-primary-headings text-lg ">Remember me</p>
+            <p className="text-primary-headings text-lg ">
+              I accept Pip-Suite{" "}
+              <span className="underline cursor-pointer">
+                Terms and Conditions.
+              </span>{" "}
+            </p>
           </div>
           <div className="w-full flex flex-col justify-center items-center gap-3">
             <Buttons
               variant="mainVariant"
               styles="w-full p-1"
-              label="Sign in"
+              label="Register"
               LeftIcon={null}
               RightIcon={null}
               link=""
               type="submit"
             />
-            <p className="text-primary-headings text-lg ml-auto pr-5 cursor-pointer">
-              Forgot password?
-            </p>
           </div>
         </form>
-        <div className="flex flex-col justify-center items-center gap-5 my-8">
+        <div className="flex flex-col justify-center items-center gap-5">
           <div className="flex justify-center items-center w-full">
             <hr className="border-t border-brand-green w-full" />
             <p className="text-primary-headings text-lg w-full text-center">
@@ -118,15 +189,15 @@ const SignIn = () => {
         <div className="flex justify-center items-center">
           <div className=" w-full flex gap-2 items-center mt-5">
             <p className="text-primary-headings text-lg w-full">
-              Don't have an account yet?
+              Already have account?
             </p>
             <Buttons
               variant="secondaryVariant"
               styles="max-w-3/5 w-3/4 p-2"
-              label="Register"
+              label="Sign in"
               LeftIcon={null}
               RightIcon={null}
-              link="/register"
+              link="/signin"
               type=""
             />
           </div>
@@ -136,9 +207,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
-
-// add quote beneath login check
-// add icons before inputs and after on PW check
-// add forgot password? beneath button check
-// add login with google and FB
+export default Register;
