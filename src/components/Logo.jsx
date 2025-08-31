8781c2e1db024d5f7c3cfcca5b72526c8018f61f
jabezdubale <jabezdubale6@gmail.com>
2025-08-31 10:18:44 +0300

    more edits on the navbar. Editted out the search bar

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/images/Logo-alone.png";
 
 const Logo = ({ size = "h-[48px]", style = "text-lg" }) => {
   return (
-    <div className="flex flex-wrap items-center justify-center gap-2 sm:w-full">
+    <div className="flex flex-wrap items-center justify-center sm:justify-start gap-2 sm:w-full">
       <img src={logo} alt="logo" className={`${size}`} />
       <p className={`${style} text-primary-headings`}>PipSuite</p>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,10 +32,18 @@ const Navbar = () => {
     <div className="px-4 h-full flex flex-col justify-between text-primary-headings text-lg">
       <div className="flex justify-between items-center gap-4 h-full">
         <div className="flex justify-start items-center gap-1 w-[80%] ">
-          <Link to={"/"} className="w-full">
+          <div
+            className={`${
+              overlaySidebarOpen ? "hidden" : ""
+            } sm:hidden pb-1 pl-[5%]`}
+            onClick={setOverlaySidebarOpen}
+          >
+            <GoSidebarCollapse size={24} />
+          </div>
+          <Link to={"/"} className="w-full ml-[5%] sm:ml-[20%]">
             <Logo size="h-8" style="text-lg gap-1" />
           </Link>
-          <div className="relative border border-primary-headings rounded-lg w-full">
+          {/* <div className="relative border border-primary-headings rounded-lg w-full">
             <IoIosSearch
               size={22}
               className="absolute top-1/2 -translate-y-1/2 left-1 text-gray-400 "
@@ -44,9 +52,9 @@ const Navbar = () => {
               className="flex items-center py-1 pl-7 w-full"
               placeholder="Search..."
             />
-          </div>
+          </div> */}
         </div>
-        <div className="flex flex-wrap justify-center items-center gap-2 w-[45%]">
+        <div className="flex flex-wrap justify-center items-center gap-2 w-[65%]">
           <div
             onClick={() => setAmountOverlayOpen()}
             className="relative flex justify-center w-20 items-center gap-2 py-1 px-2 bg-secondary-Background rounded-xl"
@@ -77,12 +85,6 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div
-        className={`${overlaySidebarOpen ? "hidden" : ""} sm:hidden pb-1`}
-        onClick={setOverlaySidebarOpen}
-      >
-        <GoSidebarCollapse size={24} />
       </div>
     </div>
   );
